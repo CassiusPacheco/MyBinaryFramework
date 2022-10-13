ce90b3b3f241e7bc13f6385b70d20bd580b6e634
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-10-13 18:33:58 +1100

    v1

--- Sources/MyBinaryFramework/MyBinaryFramework.swift
+++ Sources/MyBinaryFramework/MyBinaryFramework.swift
@@ -1,6 +1,15 @@
-public struct MyBinaryFramework {
-    public private(set) var text = "Hello, World!"
+import Valet
 
-    public init() {
+public struct MyBinaryFramework {
+    private static let shared = MyBinaryFramework()
+
+    let valet = Valet.valet(with: Identifier(nonEmpty: "MyBinaryFramework")!, accessibility: .whenUnlocked)
+
+    public static func save(_ string: String, forKey key: String) throws {
+        try shared.valet.setString(string, forKey: key)
+    }
+
+    public static func load(forKey key: String) throws -> String {
+        try shared.valet.string(forKey: key)
     }
 }
